572683131e3c69f81e87283ce5c13565304e93dc
ms4411 <myselftry0o0@gmail.com>
2026-07-12 16:03:24 +0900

    Chore : 컨트롤러 작성

--- src/main/java/com/example/demo/book/BookRepository.java
+++ src/main/java/com/example/demo/book/BookRepository.java
@@ -2,5 +2,8 @@ package com.example.demo.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional findById(Long id);
 }
--- src/main/java/com/example/demo/book/BookService.java
+++ src/main/java/com/example/demo/book/BookService.java
@@ -1,4 +1,16 @@
 package com.example.demo.book;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class BookService {
+    final private BookRepository bookRepository;
+    public String bookCreate(String name, int price){
+        Book book=new Book(name, price);
+        bookRepository.save(book);
+        return "책 생성이 성공되었습니다";
+    }
+
 }
