5476aff58da9498b3c966a25538e272fa58a175e
ms4411 <myselftry0o0@gmail.com>
2026-07-19 21:39:19 +0900

    ㅁ

---
+++ src/main/java/com/example/demo/user/presentation/DTO/SignInDTO.java
@@ -0,0 +1,4 @@
+package com.example.demo.user.presentation.DTO;
+
+public record signInDTO() {
+}
---
+++ src/main/java/com/example/demo/user/presentation/UserController.java
@@ -0,0 +1,17 @@
+package com.example.demo.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    final private UserService userService;
+    @PostMapping
+    public void signUp(){
+        
+    }
+}
