0c9aab4b20b5e5f56eca1dcc27d6e27a5621c86d
ms4411 <myselftry0o0@gmail.com>
2026-07-27 13:37:01 +0900

    jwt필터 구현

--- src/main/java/com/example/demo/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/demo/global/security/jwt/JwtTokenProvider.java
@@ -3,18 +3,26 @@ package com.example.demo.global.security.jwt;
 import com.example.demo.domain.user.refreshToken.RefreshToken;
 import com.example.demo.domain.user.refreshToken.RefreshTokenRepository;
 import com.example.demo.global.response.dto.ResponseTokenDTO;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.crypto.SecretKey;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.example.demo.global.security.SecurityConfig.hashToken;
+import static com.example.demo.global.security.jwt.jwtFilter.AUTHORIZATION_HEADER;
+import static com.example.demo.global.security.jwt.jwtFilter.BEARER_PREFIX;
 
 @Component
 public class JwtTokenProvider {
@@ -22,6 +30,7 @@ public class JwtTokenProvider {
     private final RefreshTokenRepository refreshTokenRepository;
     private final Long expAccessTime=1000L*60*30;
     private final Long expRefreshTime=1000L*60*60*24*7;
+    private static final String AUTHORITIES_KEY = "rols";
     private final JwtProperties jwtProperties;
     public JwtTokenProvider(
             JwtProperties jwtProperties,
@@ -80,5 +89,62 @@ public class JwtTokenProvider {
 
     public String getSub(String token){
         return jwtParser(token).getSubject();
+    }
+
+    //jwt필터에서 사용하는 메서드
+    //헤더에서 토큰값만 추출, 없거나 틀리면 null
+    private String resolveToken(HttpServletRequest request) {
+        // Authorization: 토큰 형태의 문자열을 읽음
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+
+        // 값에 내용이 있고, "Bearer "로 시작하는지 검증합니다.
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            // "Bearer " (7자) 이후의 토큰 값만 잘라내어 반환
+            return bearerToken.substring(BEARER_PREFIX.length());
+        }
+
+        return null; // 헤더에 토큰이 없거나 잘못된 형식인 경우 null 반환
+    }
+    //jwt 토큰 검증
+    public boolean validateToken(String token) {
+        try {
+            // 토큰 파싱 시도 (비밀키로 서명 검증 및 만료 시간 확인)
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            throw new IllegalArgumentException("잘못된 JWT 서명입니다.", e);
+        } catch (ExpiredJwtException e) {
+            throw new IllegalArgumentException("만료된 JWT 토큰입니다.", e);
+        } catch (UnsupportedJwtException e) {
+            throw new IllegalArgumentException("지원되지 않는 JWT 토큰입니다.", e);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("JWT 토큰이 잘못되었습니다.", e);
+        }
+    }
+
+    // JWT 토큰에서 Authentication 객체 추출
+    public Authentication getAuthentication(String accessToken) {
+        // 토큰 복호화하여 내부 Claim 추출
+        Claims claims = jwtParser(accessToken);
+
+        if (claims.get(AUTHORITIES_KEY) == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        // 클레임에서 권한 정보 가져오기 (예: "ROLE_USER,ROLE_ADMIN")
+        //SimpleGrantedAuthority으로 이루어진 리스트(authorities)에 저장
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        // UserDetails 객체를 만들어 Authentication 리턴
+        //(DB 조회 없이 토큰 정보만으로 생성)
+        //UserDetails : 스프링 시큐리티에서 "사용자 정보"를 다루기 위한 표준 규격
+        //(인터페이스)
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+
+        //UsernamePasswordAuthenticationToken: Authentication를 구현한 실체 클래스
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
     }
 }
---
+++ src/main/java/com/example/demo/global/security/jwt/jwtFilter.java
@@ -0,0 +1,51 @@
+package com.example.demo.global.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class jwtFilter extends OncePerRequestFilter {
+
+    // HTTP 요청 헤더에서 인증 정보를 담을 헤더 이름
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    // 토큰 접두사 (Bearer 방식 사용)
+    public static final String BEARER_PREFIX = "Bearer ";
+    // JWT 토큰 생성, 검증 및 Authentication 객체 생성을 담당하는 컴포넌트
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        // 1. 해더에서 토큰 추출. 없으면 null반환
+        String jwt = jwtTokenProvider.resolveToken(request);
+
+        // 2-1 토큰이 존재하고 유효성 검증을 통과한 경우에만 처리합니다.
+        if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+            //StringUtils.hasText(String) = 문자열이 null과 ""이 아닌경우 true
+
+            //2-2 토큰의 정보를 추출 후 시큐리티 내부 저장소에 저장
+            // 토큰 내부 데이터(Claims)를 기반으로 Security용 Authentication 객체를 만듭니다.
+            Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+            // SecurityContextHolder에 Authentication 객체를 저장합니다.
+            // 이 작업을 거치면 해당 요청(Request) 동안 컨트롤러 등에서 로그인된 사용자 정보를 조회할 수 있게 됩니다.
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        // 3. 다음 필터로 요청을 전달
+        // 토큰이 없거나 유효하지 않아도 다음 필터로 넘어간 뒤, Spring Security 설정에 따라 접근 거부(401/403) 처리됩니다.
+        filterChain.doFilter(request, response);
+    }
+}
