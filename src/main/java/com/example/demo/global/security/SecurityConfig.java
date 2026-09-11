9cd1f3493f2fe74879b213a79c835c2893a3a25d
ms4411 <myselftry0o0@gmail.com>
2026-09-11 22:23:02 +0900

    feat::csrf작성
    /user/** 경로에 대하여 모두 허용
    /book/** 경로에 대하여 로그인 사용자만 허용하나 새로운 book데이터 생성시 ROLE_MANAGER 권한 필요

--- src/main/java/com/example/demo/global/security/SecurityConfig.java
+++ src/main/java/com/example/demo/global/security/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.example.demo.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.codec.Hex;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -14,5 +17,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        http.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user/**").permitAll()
+                        .requestMatchers(HttpMethod.POST , "/book").hasRole("MANAGER")
+                        .requestMatchers("/book/**").authenticated()
+                );
+
+        return http.build();
     }
 }
